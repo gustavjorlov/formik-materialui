f743f6be96e3db23ec116689d7277d12ad1b3293
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-04-12 23:51:13 +0200

    Kind of nice way to make forms marry with material ui

--- src/App.js
+++ src/App.js
@@ -1,59 +1,101 @@
 import React, { Component } from "react";
-import { Formik } from "formik";
+import { Formik, Field } from "formik";
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import * as yup from "yup";
+
+const MICTextField = ({ field, form: { touched, errors }, ...props }) => {
+  console.log({ field, touched, errors, props });
+  return (
+    <TextField
+      {...field}
+      label={props.label}
+      helperText={errors[field.name]}
+      error={touched[field.name] && !!errors[field.name]}
+      value={field.value}
+      onBlur={field.onBlur}
+      onChange={field.onChange}
+    />
+  );
+};
 
 class App extends Component {
   render() {
     const validationSchema = yup.object().shape({
-      name: yup.string().required(),
-      email: yup.string().email()
+      password: yup.string().required(),
+      email: yup
+        .string()
+        .required()
+        .email(),
+      age: yup.number().min(10, "Age must be numeric and at least 10"),
+      computer: yup.string().required()
     });
     return (
-      <div className="App">
+      <Card>
         <h2>Hey ho</h2>
         <Formik
-          initialValues={{ email: "", password: "" }}
-          // validate={values => {
-          //   console.log("validating", values);
-          // }}
+          initialValues={{ email: "", password: "", computer: "" }}
           validationSchema={validationSchema}
           onSubmit={(values, { setSubmitting }) => {
             console.log("submitting", values);
           }}
         >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <input
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-              {errors.email && touched.email && errors.email}
-              <input
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-              />
-              {errors.password && touched.password && errors.password}
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </form>
-          )}
+          {props => {
+            const {
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting
+            } = props;
+            return (
+              <React.Fragment>
+                <form>
+                  <TextField
+                    name="email"
+                    label="Email"
+                    error={errors.email && touched.email}
+                    helperText={errors.email && touched.email && errors.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  <Field
+                    name="age"
+                    render={({ field, form }) => {
+                      console.log(field, form.errors);
+                      return (
+                        <TextField
+                          {...field}
+                          label="Age"
+                          error={!!form.errors.age}
+                          helperText={form.errors.age}
+                        />
+                      );
+                    }}
+                  />
+                  <Field
+                    name="computer"
+                    label="Computer"
+                    component={MICTextField}
+                  />
+                  <Button
+                    onClick={handleSubmit}
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Submit
+                  </Button>
+                </form>
+                <pre>{JSON.stringify({ props }, null, 2)}</pre>
+              </React.Fragment>
+            );
+          }}
         </Formik>
-      </div>
+      </Card>
     );
   }
 }
